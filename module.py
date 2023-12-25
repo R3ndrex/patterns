1c6438162eb74eb37d6d011bb2d572195d225d2a
R3ndrex <yacholohchtoli@gmail.com>
2023-12-25 11:08:53 +0200

    added module singleton production

---
+++ module.py
@@ -0,0 +1,21 @@
+class ProductionManager:
+    _instance = None
+    def __new__(cls, price, title,amount):
+        if not cls._instance:
+            cls._instance = super(ProductionManager, cls).__new__(cls)
+            cls._instance.price = price
+            cls._instance.title = title
+            cls._instance.amount = amount
+        return cls._instance
+
+product1 = ProductionManager(12.99, "Fanta", 1000)
+print("Name:", product1.price)
+print("Tail Length:", product1.title)
+print("Color:", product1.amount)
+
+product2 = ProductionManager(9.99, "Coca-Cola", 9201)
+print("Cost:", product2.price)  # 12.99
+print("Title:", product2.title)  # Fanta
+print("Amount:", product2.amount)  # 1000
+
+print(product1 is product2)  # True
